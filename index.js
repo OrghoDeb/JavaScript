1237b0b1582b6f929f5dbc52de5ee114707e2805
OrghoDeb <orghodeb@gmail.com>
2022-04-18 15:09:15 +0600

    My JavScript learning

---
+++ index.js
@@ -0,0 +1,85 @@
+
+
+
+//arrays with object
+const names = [
+  {  // 0
+  "name": "orgho",
+  "age": 20,
+  "image": "https://res.cloudinary.com/drewzxzgc/video/upload/v1643709003/wskaaeox8bvx9lpraepr.mov"
+}, 
+{ // 1
+  "name": "shama",
+  "age": 18
+},
+{ // 2
+  "name": "alak",
+  "age": 18
+}
+
+
+]
+
+// only object
+const objects = {
+  "name": "orgho",  // name is key and orgho is valuse
+  "age":  50, // age is key and 50 is value
+  "address": "sylhet"
+}
+
+//console.log(names[2].name);
+
+
+// Arrays with strings
+
+const arraysWithString =  ["orgho", "shama", "john"]
+
+
+//console.log(arraysWithString[2]);
+
+// Arryas with number 
+
+const arraysWithNumber =  [2, 4, 5]
+
+//console.log(arraysWithNumber[2])
+
+
+// Boolean It means True and False
+// const name="Bhaskar";
+
+// // const Sentence= `My name is ${name} and I love my sister`;
+// // console.log(Sentence);
+
+// if (name === 'alak') {
+//   console.log("hello alak")
+// }
+// else {
+//     console.log("Who are you??")
+// }
+
+// if age is more than 18 you are allow to drink else drink juice
+// if age is under 15 then print Go home kid
+// if age is under 10 then print why you are outside
+
+const age = 9;
+if (age>=18) {
+    console.log("Drink as much as you want")
+}
+else if (age<=15 && age >=10){ 
+    console.log("Go home kid")
+}
+else {
+    console.log("Why are you outside?")
+}
+
+
+/// True and false 
+//1. if one is true and another is false === false
+// 2. if one is true o
+
+
+// How to upload code in Github?
+
+// step one:  in terminal type git init
+// step two:  git add . 
+// step 3:  git commit -m "My javascript learning"
