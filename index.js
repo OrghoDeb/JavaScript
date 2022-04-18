60aab4e1e9c50ace577d888b0598568628989bfe
OrghoDeb <orghodeb@gmail.com>
2022-04-18 23:43:39 +0600

    Date

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-const temp=70
-if (temp>35 & temp<=135){
-  console.log("It's hot in here!!")
-}
-else if (temp>25 & temp<=35){
-  console.log("It's mildly hot")
-}
-else if(temp>18 & temp<=25){
-  console.log("It's a nice weather")
-}
-else if(temp>=10 & temp<=18){
-  console.log("It's kinda cold")
-}
-else {
-  console.log ("It's freeeeezziiingggg")
-}
+let d
+d=new Date(1999, 02, 03)
+const yr=d.getFullYear()
+const mnth=d.getMonth()
+const day=d.getDay()
+console.log("Hi! My date of birth is", yr,"-", mnth,"-", day)
---
+++ weather updtes.js
@@ -0,0 +1,16 @@
+const temp=25.4
+if (temp>35 & temp<=135){
+  console.log("It's hot in here!!")
+}
+else if (temp>25 & temp<=35){
+  console.log("It's mildly hot")
+}
+else if(temp>18 & temp<=25){
+  console.log("It's a nice weather")
+}
+else if(temp>=10 & temp<=18){
+  console.log("It's kinda cold")
+}
+else {
+  console.log ("It's freeeeezziiingggg")
+}
