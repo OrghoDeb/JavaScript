f459f801cfeebcd1a51607e73a06d951db740d6d
OrghoDeb <orghodeb@gmail.com>
2022-04-25 14:59:45 +0600

    date.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let d
-d=new Date(1999, 02, 03)
-const yr=d.getFullYear()
-const mnth=d.getMonth()
-const day=d.getDay()
-console.log("Hi! My date of birth is", yr,"-", mnth,"-", day)
+d=new Date()
+console.log(d)
+let yesterday
+yesterday=date.getdate
+console.log(yesterday)
