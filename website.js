cec624b31767798cc8d1c95906e5e993f6269a8a
OrghoDeb <orghodeb@gmail.com>
2022-05-11 10:00:04 +0600

    Shortcut of name, email & password Validation :tada:

--- website.js
+++ website.js
@@ -1,7 +1,7 @@
 const firstName="Bhaskar"
 const lastName= "Deb"
 
-function nameShortcut(firstName, lastName){
+function nameShortcut(firstName, middleName, lastName){
     return firstName.substr(0, 1) + lastName.substr(0,1)
 }
 
@@ -32,6 +32,6 @@ function passwordValidation (password, confirmPassword)
     }
 }
 
-console.log(`Hello`, nameShortcut("Bhaskar", "Deb")+"!", `Welcome to Google.com.`)
+console.log(`Hello`, nameShortcut("Alak", "Kumar", "Dam")+"!", `Welcome to Google.com.`)
 console.log("Here as you can see", emailValidation("orghodeb@gmail.com","orghodeb@gmail.com")+", and", passwordValidation("Limit","Limit")+"!")
 console.log("So now you don't have anything to worry about! You're all good to go! Enjoy Wasting our free storage")
