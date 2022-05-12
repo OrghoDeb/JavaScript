b08bb91584507f1da8826f6013e2d46af7c11ea7
OrghoDeb <orghodeb@gmail.com>
2022-05-13 01:26:58 +0600

    The first letter of the name has to be in Capital Letter :tada:

--- firstCharacterCapital.js
+++ firstCharacterCapital.js
@@ -1,13 +1,24 @@
-function firstCharacterCapital (firstName, lastName){
-  return firstName.charAt(0).toUpperCase() + firstName.slice(1) + " " +
-  lastName.charAt(0).toUpperCase() + lastName.slice(1)}
-console.log(`Hello ${firstCharacterCapital("bhaskar", "deb")}`);
+// function firstCharacterCapital (firstName, lastName){
+//   return firstName.charAt(0).toUpperCase() + firstName.slice(1) + " " +
+//   lastName.charAt(0).toUpperCase() + lastName.slice(1)}
+// console.log(`Hello ${firstCharacterCapital("bhaskar", "deb")}`);
 
 
 // Another Way
 function nameCharacterCapital (name){
-    return name.split(' ').slice(0,-1).join('').charAt(0).toUpperCase()+
+    return name.split(' ').slice(0, 1, -1).join('').charAt(0).toUpperCase()+
     name.split(' ').slice(0,-1).join('').slice(1)+ " " + name.split(' ').slice(-1).join('').charAt(0).toUpperCase()+
     name.split(' ').slice(-1).join('').slice(1)
   }
   console.log(`Hello ${nameCharacterCapital("bhaskar deb")}`);
+
+  //First character of Each Word in capital
+  function capital(name){
+    const array = name.toLowerCase().split (" ")
+    const titleCase = array.map(function(val){
+      return val.replace(val.charAt(0), val.charAt(0).toUpperCase())
+    })
+    return titleCase.join(' ')
+  }
+
+  console.log(`Hello ${capital("rajib kumar deb")}`)
