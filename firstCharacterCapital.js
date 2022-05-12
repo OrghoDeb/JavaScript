09eb57e048ef23e0dcbf0ab38e59cd2205b02036
OrghoDeb <orghodeb@gmail.com>
2022-05-13 00:23:34 +0600

    The first letter of the name has to be in Capital Letter :tada:

--- firstCharacterCapital.js
+++ firstCharacterCapital.js
@@ -7,7 +7,7 @@ console.log(`Hello ${firstCharacterCapital("bhaskar ", "deb")}`);
 // Another Way
 function nameCharacterCapital (name){
     return name.split(' ').slice(0,-1).join('').charAt(0).toUpperCase()+
-    name.split(' ').slice(0,-1).join('').slice(1) + name.split(' ').slice(-1).join('').charAt(0).toUpperCase()+
+    name.split(' ').slice(0,-1).join('').slice(1)+ " " + name.split(' ').slice(-1).join('').charAt(0).toUpperCase()+
     name.split(' ').slice(-1).join('').slice(1)
   }
   console.log(`Hello ${nameCharacterCapital("bhaskar deb")}`);
